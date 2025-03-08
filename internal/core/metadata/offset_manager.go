18352440277a3fcb3a3c931803052173cc099474
Adwin2 <1528411608@qq.com>
2025-03-08 21:44:55 +0800

    Add tutorial/

--- internal/core/metadata/offset_manager.go
+++ internal/core/metadata/offset_manager.go
@@ -1,4 +1,6 @@
 /*
- 持久化消费者Offset到 LevelDB/BoltDB
- 提供Commit(Offset) 和 Fetch()接口
+
+持久化消费者Offset到 BoltDB
+提交/获取消费(Offset)进度 : Commit(Offset) 和 Fetch()接口
+? 创建/列出 消费者组 : CreateGroup() 和 ListGroups()接口  ？
 */
--- internal/core/metadata/partition_meta.go
+++ internal/core/metadata/partition_meta.go
@@ -4,6 +4,8 @@
  记录Leader/Follower 节点 （分布式需求）
  维护ISR列表(In-Sync Replica)
 
+ 追踪分区状态
+
  ​关键技术：
  用嵌入式数据库（如 BoltDB）存储元数据
 */
