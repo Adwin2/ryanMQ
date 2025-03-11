167e62ef99f864ac2a77a1a669605cc7c2382300
Adwin2 <1528411608@qq.com>
2025-03-11 22:24:34 +0800

    tutorial/v2

--- tutorial/v1/mq/broker.go
+++ tutorial/v1/mq/broker.go
@@ -1,5 +1,25 @@
 package mq
 
+import "sync"
+
 type Broker struct {
 	topics map[string]*Topic
+	mu     sync.RWMutex
+}
+
+func NewBroker() *Broker {
+	return &Broker{
+		topics: make(map[string]*Topic),
+	}
+}
+
+func (b *Broker) CreateTopic(name string) *Topic {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if topic, exists := b.topics[name]; exists {
+		return topic
+	}
+
+	topic := NewTopic
 }
